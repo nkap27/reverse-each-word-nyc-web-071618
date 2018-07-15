e15ce58415b6761ff7231109a16b0334c9ec615c
Natasha K <kapr.natasha@gmail.com>
2018-07-15 21:11:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
-def reverse_each_word(sentence1)
-  sentence1 = sentence1.split
-  string = []
-
-  sentence1.each do |word|
-    string << word.reverse
-  end
-  return string.join(' ')
-end
-
-# def reverse_each_word(sentence2)
-#   sentence2 = sentence2.split
+# def reverse_each_word(sentence1)
+#   sentence1 = sentence1.split
+#   string = []
 #
-#   sentence2.collect do |word|
-#     word.reverse
+#   sentence1.each do |word|
+#     string << word.reverse
 #   end
-#   return sentence2.join(' ')
+#   return string.join(' ')
 # end
+
+def reverse_each_word(sentence2)
+  sentence2 = sentence2.split
+
+  sentence2.collect do |word|
+    word.reverse
+  end
+  return sentence2.join(' ')
+end
