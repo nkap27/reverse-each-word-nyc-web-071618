062261ab3d2121afc9caa43ed91d56b4f08e76dd
Natasha K <kapr.natasha@gmail.com>
2018-07-15 21:06:39 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word(sentence1)
+  sentence1 = sentence1.split
+  string = []
+
+  sentence1.each do |word|
+    string << word.reverse
+  end
+  string.join(' ')
+end
+
+def reverse_each_word(sentence2)
+  sentence2 = sentence2.split
+
+  sentence2.collect do |word|
+    word.reverse
+  end
+  sentence2
+end
